1261ea21f43d2966715336ec27109eb4b47bdc34
Sam Russell <gnarlynarwhal8@gmail.com>
2016-12-15 11:18:28 -0800

    Removed friction and acceleration

--- src/com/cs/game/Main.java
+++ src/com/cs/game/Main.java
@@ -1,12 +1,10 @@
 package com.cs.game;
 
 import static org.lwjgl.glfw.GLFW.*;
-
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.ArrayList;
 import org.joml.Matrix4f;
-
 import com.cs.engine.GameObject;
 import com.cs.engine.Window;
 import com.cs.engine.graphics.ShaderLibrary;
@@ -16,53 +14,41 @@ public class Main {
 	Window window;
 	ShaderLibrary shaders;
 	boolean running = true;
-	Map<String, GameObject> objects = new HashMap<>();
-	GameObject block1;
-	GameObject block2;
+	Map<String, GameObject> objects = new HashMap<>(); //Stores an object with a mesh but isn't rendered
+	ArrayList<GameObject> instances = new ArrayList<>(); //Stores the instances of the objects and is what gets rendered
 	public static Matrix4f projection;
 	
 	public Main() {
-		window = new Window(1000, 700, true);
+		window = new Window(0, 0, true);
 		shaders = new ShaderLibrary();
 		projection = new Matrix4f().ortho2D(-window.getWidth() / 2, window.getWidth() / 2, -window.getHeight() / 2, window.getHeight() / 2);
 		run();
 	}
 	
 	public void run() {
-		objects.put("Stone Block", new GameObject("Stone Block", shaders.getDefault()));
-		objects.put("Spring", new GameObject("Spring", shaders.getDefault(), 4, 2, true));
-		block1 = objects.get("Stone Block").newInstanceOf(0.0f, 0.0f, 0.0f, 0.0f, 8.0f, 1.0f, 0.1f, false);
 		while(running) {
 			input();
-			update(block1);
-			render(block1);
+			update();
+			render();
 			running = !window.shouldClose();
 		}
+		window.cleanup();
 	}
 	
 	public void input() {
-		float force = 0.05f;
-		if(window.isKeyPressed(GLFW_KEY_D))      block1.addForce(0,   force);
-		if(window.isKeyPressed(GLFW_KEY_W))      block1.addForce(90,  force);
-		else if(window.isKeyPressed(GLFW_KEY_A)) block1.addForce(180, force);
-		else if(window.isKeyPressed(GLFW_KEY_S)) block1.addForce(270, force);
-		if(window.isKeyPressed(GLFW_KEY_RIGHT))     block2.addForce(0,   force);
-		if(window.isKeyPressed(GLFW_KEY_UP))        block2.addForce(90, force);
-		else if(window.isKeyPressed(GLFW_KEY_LEFT)) block2.addForce(180,  force);
-		else if(window.isKeyPressed(GLFW_KEY_DOWN)) block2.addForce(270, force);
-		if(window.isKeyPressed(GLFW_KEY_ESCAPE)) window.close();
+		/*EXAMPLE: if(window.isKeyPressed(GLFW_KEY_*insert key name*) ...; */
 	}
 	
-	public void update(GameObject... gameObjects) {
+	public void update() {
 		window.update();
-		for (int i = 0; i < gameObjects.length; i++)
-			gameObjects[i].update();
+		for (int i = 0; i < instances.size(); i++)
+			instances.get(i).update();
 	}
 	
-	public void render(GameObject... gameObjects) {
+	public void render() {
 		window.clear();
-		for (int i = 0; i < gameObjects.length; i++)
-			gameObjects[i].render();
+		for (int i = 0; i < instances.size(); i++)
+			instances.get(i).render();
 		window.swap();
 	}
 	
